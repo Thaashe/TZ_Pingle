6c15280216de6f48f7f7b02aec1817abd14b4071
Thaashe <thaashe@gmail.com>
2019-12-20 00:22:00 +0200

    UI button "Play"

---
+++ Assets/_Project/Scripts/StartGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class StartGame : MonoBehaviour
+{
+	public FirstPersonController firstPersonController;
+	public GameObject UIObject;
+	public GameObject playViewCamera;
+	public GameObject mainCamera;
+
+    public void OnUIButtonClick()
+	{
+		playViewCamera.SetActive(false);
+		mainCamera.SetActive(true);
+
+		firstPersonController.enabled = true;
+		UIObject.SetActive(false);
+	}
+}
