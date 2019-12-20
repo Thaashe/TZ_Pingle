1fa25a373e833757bcefef0b7f23f76e2837ca76
Thaashe <thaashe@gmail.com>
2019-12-20 13:34:37 +0200

    active buttons + cinemachine

---
+++ Assets/_Project/Scripts/DemoScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.Playables;
+
+
+public class DemoScene : MonoBehaviour
+{
+	public FirstPersonController firstPersonController;
+	public GameObject UIObject;
+
+	public PlayableDirector playableDirector;
+	public GameObject cinemachineTimeline;
+	
+	public void OnUIButtonClick()
+	{
+		UIObject.SetActive(false);
+		playableDirector.stopped += OnPlayableDirectorStopped;
+		cinemachineTimeline.SetActive(true);
+		playableDirector.Play();
+	}
+
+	void OnPlayableDirectorStopped(PlayableDirector aDirector)
+	{
+		UIObject.SetActive(true);
+		cinemachineTimeline.SetActive(false);
+	}
+}
